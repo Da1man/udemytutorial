f4a77058a70a327ba1a10234e8418872e28a0bfe
Da1man <chaos4ever@mail.ru>
2020-01-12 18:45:07 +0300

    lesson 28 done

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import {THEME} from '../theme'
+import {AppCard} from '../components/ui/AppCard'
 
 
 export const TodoScreen = ({goBack, todo}) => {
   return (
     <View>
-      <Text>{todo.title}</Text>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title={'Ред'} onPress={() => console.log('Button pressed')} />
+      </AppCard>
+
       <View style={styles.buttons}>
         <View style={styles.button}>
           <Button
@@ -32,5 +37,12 @@ const styles = StyleSheet.create({
   },
   button: {
     width: '40%'
+  },
+  title: {
+    fontSize: 20,
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15,
   }
 });
