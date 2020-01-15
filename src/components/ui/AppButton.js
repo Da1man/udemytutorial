b85e5ff6105b9ac288c67de308ec45eff4cf866a
Da1man <chaos4ever@mail.ru>
2020-01-15 12:02:28 +0300

    lesson 39 done

--- src/components/ui/AppButton.js
+++ src/components/ui/AppButton.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, TouchableOpacity, TouchableNativeFeedback, Platform} from 'react-native';
 import {AppTextBold} from './AppTextBold';
 import {THEME} from '../../theme';
 
-export const AppButton = ({ children, onPress, color = THEME.MAIN_COLOR }) => {
+export const AppButton = ({children, onPress, color = THEME.MAIN_COLOR}) => {
+  const Wrapper = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity
   return (
-    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+    <Wrapper onPress={onPress} activeOpacity={0.7}>
       <View style={{...styles.button, backgroundColor: color}}>
         <AppTextBold style={styles.text}>{children}</AppTextBold>
       </View>
-    </TouchableOpacity>
+    </Wrapper>
   );
 };
 
@@ -20,9 +21,9 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   text: {
-    color: '#fff'
-  }
+    color: '#fff',
+  },
 });
